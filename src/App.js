17a6e11c3fa93af10224ab52cae774fba25c0472
musasazib <musasazib@gmail.com>
2021-09-22 01:05:01 +0600

    State, Component State, Set State Method

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter></Counter>
     </div>
   );
 }
 
+function Counter() {
+  const [count, setCount] = useState(67);
+  const handleIncrease = () => {
+    const newCount = count + 1;
+    setCount(newCount);
+  }
+  const handleDecrease = () => {
+    const newCount = count - 1;
+    setCount(newCount);
+  }
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={handleIncrease}>Increase</button>
+      <button onClick={handleDecrease}>Decrease</button>
+    </div>
+  )
+}
+
+function Product(props) {
+  const productStyle = {
+    margin: '10px'
+  }
+  return (
+    <div className="product" style={productStyle}>
+      <h2>Name: {props.name}</h2>
+      <h4>Price: {props.Price}</h4>
+    </div>
+  )
+}
+
 export default App;
+
+/* const products = [
+    {name: 'Mobile', Price: 22000},
+    {name: 'Computer', Price: 50000},
+    {name: 'Book', Price: 500},
+    {name: 'Water', Price: 100},
+    {name: 'Pen', Price: 30},
+  ]*/
